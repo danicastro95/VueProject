8043a2cf9209283295c36c9d41b28a165b730362
Daniel Castro <34897196+danicastro95@users.noreply.github.com>
2019-01-24 23:59:03 +0100

    Datos de las notas

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -19,11 +19,77 @@ export default {
   },
   methods: {
     addNote: function(event) {
-      this.notes.push({title: event.target.value});
-      event.target.value = '';
+      let note = {
+        title: event.target.value,
+        priority: 1,
+        time: new Date().toDateString(),
+        done: false
+      };
+      this.notes.push(note);
+      event.target.value = "";
+      localStorage.setItem("notes", JSON.stringify(this.notes));
     }
   }
 };
+
+Vue.component("button-counter", {
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  template:
+    '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    newTodoText: "",
+    items: [
+      {
+        tarea: "Tarea 1",
+        prioridad: "ALTA",
+        fecha_creacion: new Date().getDate(),
+        completada: false
+      },
+      {
+        tarea: "Tarea 2",
+        prioridad: "MEDIA",
+        fecha_creacion: new Date().getDate(),
+        completada: true
+      }
+    ]
+  },
+  methods: {
+    addNewTodo: function() {
+      nuevaTarea = {
+        tarea: this.newTodoText,
+        prioridad: "BAJA",
+        fecha_creacion: new Date().getDate(),
+        completada: false
+      };
+      this.items.push(nuevaTarea);
+      this.newTodoText = "";
+      localStorage.setItem("todos", JSON.stringify(this.items));
+    }
+  },
+  computed: {
+    completadas: function() {
+      let list;
+      list = this.items.filter(function(item) {
+        return item.completada;
+      });
+      return list.length;
+    }
+  },
+  mounted() {
+    var items = JSON.parse(localStorage.getItem("todos"));
+    if (items) {
+      this.items = items;
+    }
+  }
+});
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
