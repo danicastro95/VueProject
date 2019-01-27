11f2b9ade2b52827cb73f9f80dcfad11393e7a6f
Daniel Castro <34897196+danicastro95@users.noreply.github.com>
2019-01-27 20:25:13 +0100

    Production-ready

---
+++ js/chunk-2d0d9f26.60dc1f0d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d9f26"],{"6a05":function(t,e,n){"use strict";n.r(e);var c=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},r=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about text-light"},[n("h1",[t._v("Under construction")])])}],s=n("2877"),u={},a=Object(s["a"])(u,c,r,!1,null,null,null);a.options.__file="Weather.vue";e["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0d9f26.60dc1f0d.js.map
